c50c51a72866fe917198b68ef2d2732ebe5c0ce6
DeFox67 <abdul.arifin6767@gmail.com>
2025-08-30 02:02:23 +0700

    make relationship in model

--- app/Models/ProductSubscription.php
+++ app/Models/ProductSubscription.php
@@ -4,8 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductSubscription extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable =[
+        'name',
+        'phone',
+        'email',
+        'proof',
+        'booking_trx_id',
+        'total_amount',
+        'total_tax_amount',
+        'customer_bank_name',
+        'customer_bank_account',
+        'customer_bank_number',
+        'is_paid',
+        'duration',
+        'price',
+        'product_id',
+        
+    ];
+
+    public function product():BelongsTo
+    {
+        return $this->belongsTo(Product::class,'prodoct_id');
+    }
+
+    public function group():HasOne
+    {
+        return $this->hasOne(SubscriptionGroup::class,'prodoct_subscription_id');
+    }
 }
--- app/Models/SubscriptionGroup.php
+++ app/Models/SubscriptionGroup.php
@@ -4,8 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SubscriptionGroup extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+        protected $fillable =[
+        'product_id',
+        'product_subscription_id',
+        'max_capacity',
+        'participant_count',
+        ];
+
+        public function product():BelongsTo
+        {
+            return $this->belongsTo(Product::class,'product_id');
+        }
+
+        public function productSubscription():BelongsTo
+        {
+            return $this->belongsTo(ProductSubscription::class,'product_subscription_id');
+        }
+
+        public function groupMessage():HasMany
+        {
+            return $this->hasMany(GroupMessage::class)->latest();
+        }
+
+        public function groupParticipant():HasMany
+        {
+            return $this->hasMany(GroupParticipant::class);
+        }
 }
